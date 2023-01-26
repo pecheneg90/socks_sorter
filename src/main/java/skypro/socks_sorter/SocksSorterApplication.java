0e42530e2d9da0816e4d150ef0bc127621288199
pecheneg90 <manjavha@gmail.com>
2023-01-26 14:44:09 +0300

    Initial commit

---
+++ src/main/java/skypro/socks_sorter/SocksSorterApplication.java
@@ -0,0 +1,13 @@
+package skypro.socks_sorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksSorterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocksSorterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/socks_sorter/SocksSorterApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.socks_sorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksSorterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
